871c12d860bcbfb16352b3bb0449c4c6b624a5be
GiorgioBrux <giorgiobrux2002@gmail.com>
2019-07-29 20:41:49 +0200

    Revert "Revert "Revert "Revert "Update xmrstak""""
    This reverts commit 780700223884dde9e97628696d2d03e76ef33f1c.

--- xmrstak/version.cpp
+++ xmrstak/version.cpp
@@ -1,10 +1,11 @@
 #include "version.hpp"
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+#define GIT_ARCHIVE 1
 #if defined(GIT_ARCHIVE) && !defined(GIT_COMMIT_HASH)
 #define GIT_COMMIT_HASH \
-	$Format:            \
-	% h$
+	            \
+	 5869b9eb0
 #endif
 
 #ifndef GIT_COMMIT_HASH
@@ -20,7 +21,7 @@
 #endif
 
 #define XMR_STAK_NAME "xmr-stak"
-#define XMR_STAK_VERSION "2.10.4"
+#define XMR_STAK_VERSION "2.10.6"
 
 #if defined(_WIN32)
 #define OS_TYPE "win"
